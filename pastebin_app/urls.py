5a7bca080958aae8c30e0d7fbdd6ca1d00444f72
Ignat Bojinov <ignatbojinov@gmail.com>
2022-05-10 17:39:50 +0300

    initial commit

--- pastebin_app/urls.py
+++ pastebin_app/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path('', views.PasteList.as_view(), name='paste_list'),
     path('paste/create', views.PasteCreate.as_view(), name='create'),
     path('paste/<int:pk>', views.PasteDetail.as_view(), name='paste_detail'),
+    path('paste/edit/<int:pk>', views.PasteUpdate.as_view(), name='paste_update'),
+    path('paste/delete/<int:pk>', views.PasteDelete.as_view(), name='paste_delete'),
+
+
 
 ]
